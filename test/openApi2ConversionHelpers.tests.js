d04ae7a9d8dfc50b9c6552b394bc38e9d1e51c2c
Al Saunders <al@betterez.com>
2018-01-19 21:36:16 -0500

    [AS] Changes to support more OpenApi 2 transformations.  Refactor some code and add a few tests.  Update version

---
+++ test/openApi2ConversionHelpers.tests.js
@@ -0,0 +1,95 @@
+'use strict';
+
+describe('swagger 2 conversion helpers', function () {
+  const expect = require("chai").expect;
+  const conversionHelper = require('../lib/openApi2ConversionHelpers.js');
+
+  let sampleArrayParam = {
+
+  };
+  let sampleObjectParam = {
+
+  };
+
+  it('should convert primitive to valid OpenApi 2.0 format', function () {
+    let samplePrimitiveParam = {
+      name: "testPrimitive",
+      description: "testPrimitive id required.",
+      type: "string",
+      required: true,
+      paramType: "body"
+    };
+
+    let schemaObject = conversionHelper.getBodySchema(samplePrimitiveParam);
+
+    expect(schemaObject).to.deep.equal({ "type": "string" });
+
+  });
+
+  it('should convert array type to valid OpenApi 2.0 format', function () {
+    let sampleArrayParam = {
+      name: "testArray",
+      description: "List of testOperations to apply.",
+      type: "array",
+      required: true,
+      paramType: "body",
+      defaultValue: null,
+      items: { "$ref": "objectToPointTo" }
+    };
+
+    let schemaObject = conversionHelper.getBodySchema(sampleArrayParam);
+
+    expect(schemaObject).to.deep.equal({
+      properties:
+        {
+          sources:
+            {
+              type: 'array',
+              items: { '$ref': '#/definitions/objectToPointTo' }
+            }
+        }
+    });
+  });
+
+  it('should convert object type to valid OpenApi 2.0 format', function () {
+    let sampleObjectParam = {
+      name: "testObject",
+      description: "testObject required.",
+      type: "objectToPointTo",
+      required: true,
+      paramType: "body"
+    };
+
+    let schemaObject = conversionHelper.getBodySchema(sampleObjectParam);
+
+    expect(schemaObject).to.deep.equal({ "$ref": "#/definitions/objectToPointTo" });
+  });
+
+  it('should convert model to OpenAPI 2.0 friendly format', function () {
+    let models = {
+        "Info": {
+          "id": "Info",
+          "required": [],
+          "properties": {
+            "name": {
+              "type": "string"
+            }
+          }
+        }
+    };
+
+    conversionHelper.convertModelsToOpenApi2(models);
+
+    expect(models).to.deep.equal( {
+        "Info": {
+          "properties": {
+            "name": {
+              "type": "string"
+            }
+          }
+        }
+      }
+    );
+  });
+
+});

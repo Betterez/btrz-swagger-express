0757ef584248291bb3e539471354716201deccb7
Al Saunders <al@betterez.com>
2018-05-01 15:33:27 -0400

    [AS] Removing notes append function, as it is no longer needed

--- test/openApi2ConversionHelpers.tests.js
+++ test/openApi2ConversionHelpers.tests.js
@@ -307,26 +307,6 @@ describe('swagger 2 conversion helpers', function () {
     expect(doc).to.deep.equal(docAfterValue);
   });
 
-  it('should append operation notes to description', function () {
-    const 
-      description = "Here is the description",
-      notes = "Here are the notes";
-
-    let result = conversionHelper.appendNotesToDescription(description, notes);
-
-    expect(result).to.deep.equal("Here is the description\n\nHere are the notes");
-  });
-
-  it('should return just the description if no notes included', function () {
-    const 
-      description = "Here is the description",
-      notes = null;
-
-    let result = conversionHelper.appendNotesToDescription(description, notes);
-
-    expect(result).to.deep.equal("Here is the description");
-  });
-
   it("should convert OpenApi 1.2 syntax to OpenApi 2 syntax", () => {
     const
       key = "customers",
@@ -346,9 +326,8 @@ describe('swagger 2 conversion helpers', function () {
             "paramType": "body"
           }
         ],
-        "description": "Set a Credential to a Customer for the first time",
         "summary": "Set new Customer Credential",
-        "notes": "Sets the Credential of a Customer account that doesn't have it configured. Password must be at least 6 characters long and match the confirmation. The Credential will be pending validation (token+email).",
+        "description": "Sets the Credential of a Customer account that doesn't have it configured. Password must be at least 6 characters long and match the confirmation. The Credential will be pending validation (token+email).",
         "produces": ["text/plain"],
         "type": "string",
         "errorResponses": [
@@ -374,7 +353,7 @@ describe('swagger 2 conversion helpers', function () {
                 "customers"
               ],
               "summary": "Set new Customer Credential",
-              "description": "Set a Credential to a Customer for the first time\n\nSets the Credential of a Customer account that doesn't have it configured. Password must be at least 6 characters long and match the confirmation. The Credential will be pending validation (token+email).",
+              "description": "Sets the Credential of a Customer account that doesn't have it configured. Password must be at least 6 characters long and match the confirmation. The Credential will be pending validation (token+email).",
               "operationId": "change-customer-credential",
               "produces": [
                 "text/plain"
